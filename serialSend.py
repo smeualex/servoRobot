36c772b93ab027f70bffb4bc2285af2d59791e84
smeualex <smeualex@gmail.com>
2017-03-14 22:25:03 +0200

    Add files via upload

---
+++ serialSend.py
@@ -0,0 +1,183 @@
+import serial
+import time
+import pygame
+
+# Define some colors
+BLACK    = (   0,   0,   0)
+WHITE    = ( 255, 255, 255)
+
+
+# controller axis mappings to servo numbers
+servoDict = { 0:0, 1:1, 3:2, 4:3 }
+s = serial.Serial('COM5', 9600)
+lastValuesSent = [9999, 9999, 9999, 9999]
+
+# commands to send
+commandsToSend = []
+SEND_MAX_NUMBER = 25
+
+#data sent to Arduino is relative to the current values or absolute
+CMD_RELATIVE = 1
+CMD_ABSOLUTE = 2
+
+def mapValue(x, in_min, in_max, out_min, out_max):
+    return int((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min);
+
+def serialSend(value, servo):
+    if value <=1 and val >= -1:
+        return
+
+    #if value != lastValuesSent[servo]:
+    msg = " Sending servo " + str(servo) + " value " + str(value)
+    print(msg)
+    cmd = str(CMD_RELATIVE) + str(value) + '#' + str(servo)
+    
+    commandsToSend.append(cmd)
+    #print(commandsToSend)
+    if len(commandsToSend) >= SEND_MAX_NUMBER:
+        print('Sending data over serial: ' + ';'.join(commandsToSend))
+        s.write( bytes(';'.join(commandsToSend).encode('ascii')) )
+        commandsToSend[:]=[]
+
+    #s.write( bytes( str(CMD_RELATIVE).encode('ascii') + str(value).encode('ascii') + '#'.encode('ascii') + str(servo).encode('ascii') + ';'.encode('ascii') ) )
+    #lastValuesSent[servo] = value
+    time.sleep(0.05)
+
+# This is a simple class that will help us print to the screen
+# It has nothing to do with the joysticks, just outputing the
+# information.
+class TextPrint:
+    def __init__(self):
+        self.reset()
+        self.font = pygame.font.Font(None, 20)
+
+    def print(self, screen, textString):
+        textBitmap = self.font.render(textString, True, BLACK)
+        screen.blit(textBitmap, [self.x, self.y])
+        self.y += self.line_height
+        
+    def reset(self):
+        self.x = 10
+        self.y = 10
+        self.line_height = 15
+        
+    def indent(self):
+        self.x += 10
+        
+    def unindent(self):
+        self.x -= 10
+    
+
+pygame.init()
+ 
+# Set the width and height of the screen [width,height]
+size = [500, 700]
+screen = pygame.display.set_mode(size)
+
+pygame.display.set_caption("My Game")
+
+#Loop until the user clicks the close button.
+done = False
+
+# Used to manage how fast the screen updates
+clock = pygame.time.Clock()
+
+# Initialize the joysticks
+pygame.joystick.init()
+    
+# Get ready to print
+textPrint = TextPrint()
+
+# -------- Main Program Loop -----------
+while done==False:
+    # EVENT PROCESSING STEP
+    for event in pygame.event.get():  # User did something
+        if event.type == pygame.QUIT: # If user clicked close
+            done=True # Flag that we are done so we exit this loop
+        
+        # Possible joystick actions: JOYAXISMOTION JOYBALLMOTION JOYBUTTONDOWN JOYBUTTONUP JOYHATMOTION
+        if event.type == pygame.JOYBUTTONDOWN:
+            print("Joystick button pressed.")
+        if event.type == pygame.JOYBUTTONUP:
+            print("Joystick button released.")
+            
+ 
+    # DRAWING STEP
+    # First, clear the screen to white. Don't put other drawing commands
+    # above this, or they will be erased with this command.
+    screen.fill(WHITE)
+    textPrint.reset()
+
+    # Get count of joysticks
+    joystick_count = pygame.joystick.get_count()
+
+    textPrint.print(screen, "Number of joysticks: {}".format(joystick_count) )
+    textPrint.indent()
+    
+    # For each joystick:
+    for i in range(joystick_count):
+        joystick = pygame.joystick.Joystick(i)
+        joystick.init()
+    
+        textPrint.print(screen, "Joystick {}".format(i) )
+        textPrint.indent()
+    
+        # Get the name from the OS for the controller/joystick
+        name = joystick.get_name()
+        textPrint.print(screen, "Joystick name: {}".format(name) )
+        
+        # Usually axis run in pairs, up/down for one, and left/right for
+        # the other.
+        axes = joystick.get_numaxes()
+        textPrint.print(screen, "Number of axes: {}".format(axes) )
+        textPrint.indent()
+        
+        for i in range( axes ):
+            axis = joystick.get_axis( i )
+            textPrint.print(screen, "Axis {} value: {:>6.3f}".format(i, axis) )
+        
+            # ignore values coming from axis 2
+            if i != 2:
+                val = mapValue(axis*1000, 0, 1000, 0, 15)
+                textPrint.print(screen, "mappedValue {}".format(val))
+                serialSend(val, servoDict.get(i))
+                
+        textPrint.unindent()
+            
+        buttons = joystick.get_numbuttons()
+        textPrint.print(screen, "Number of buttons: {}".format(buttons) )
+        textPrint.indent()
+
+        for i in range( buttons ):
+            button = joystick.get_button( i )
+            textPrint.print(screen, "Button {:>2} value: {}".format(i,button) )
+                
+        textPrint.unindent()
+            
+        # Hat switch. All or nothing for direction, not like joysticks.
+        # Value comes back in an array.
+        hats = joystick.get_numhats()
+        textPrint.print(screen, "Number of hats: {}".format(hats) )
+        textPrint.indent()
+
+        for i in range( hats ):
+            hat = joystick.get_hat( i )
+            textPrint.print(screen, "Hat {} value: {}".format(i, str(hat)) )
+        textPrint.unindent()
+        
+        textPrint.unindent()
+
+    
+    # ALL CODE TO DRAW SHOULD GO ABOVE THIS COMMENT
+    
+    # Go ahead and update the screen with what we've drawn.
+    pygame.display.flip()
+
+    # Limit to 20 frames per second
+    clock.tick(20)
+    
+# Close the window and quit.
+# If you forget this line, the program will 'hang'
+# on exit if running from IDLE.
+pygame.quit ()
+
---
+++ usb_servo_ctl.ino
@@ -0,0 +1,292 @@
+#include <Servo.h>
+
+#define BUTTON_1_PIN 10
+#define BUTTON_2_PIN 16
+
+#define BTN_RED   BUTTON_1_PIN
+#define BTN_BLACK BUTTON_2_PIN
+
+const byte numChars = 64;
+char receivedChars[numChars];
+char strNewValue[4];
+char strServoNum[2];
+
+
+#define USE_RELATIVE_VALUES 1
+#define USE_ABSOLUTE_VALUES 2
+#define SERVO_MOVEMENT USE_RELATIVE_VALUES
+
+boolean newData = false;
+
+#define SERVO_PIN_0 6;
+#define SERVO_PIN_1 7;
+#define SERVO_PIN_2 8;
+#define SERVO_PIN_3 9;
+
+#define SERVO_START_ANGLE 90
+
+#define NSERVOS 4
+struct ServoHndl
+{
+#define MAX_HIST_STEPS 25
+  Servo servo;
+  int   servoPin;
+  short currentAngle;
+  
+  float microMove;
+  float microMIN;
+  float microMAX;
+  int   angleMIN;
+  int   angleMAX;
+
+  short movementDelay;
+  short movementStep;
+//  short angleHistory[MAX_HIST_STEPS];
+} servosH[NSERVOS];
+
+unsigned long currentMillis  = millis();
+unsigned long previousMillis = 0;
+
+void setup() 
+{
+  short i = 0; 
+  Serial.begin(9600);
+  
+  /* SET THE BUTTON PINS */
+  pinMode     (BTN_RED, INPUT_PULLUP);
+  digitalWrite(BTN_RED, HIGH);
+
+  pinMode     (BTN_BLACK, INPUT_PULLUP);
+  digitalWrite(BTN_BLACK, HIGH);
+
+  /* INITIALIZE THE SERVO MOTORS */
+  for( i = 0; i < NSERVOS; i++)
+  {
+    switch(i)
+    {
+      case 0:
+        servosH[i].servoPin = SERVO_PIN_0;
+        servosH[i].microMIN = 600;
+        servosH[i].microMAX = 2500;
+        servosH[i].angleMIN = 0;
+        servosH[i].angleMAX = 180;
+        servosH[i].movementDelay =  1;
+        servosH[i].movementStep = 1;
+      break;
+
+      case 1:
+        servosH[i].servoPin = SERVO_PIN_1;
+        servosH[i].microMIN = 600;
+        servosH[i].microMAX = 2500;
+        servosH[i].angleMIN = 0;
+        servosH[i].angleMAX = 180;
+        servosH[i].movementDelay =  1;
+        servosH[i].movementStep = 1;
+      break;
+
+      case 2:
+        servosH[i].servoPin = SERVO_PIN_2;
+        servosH[i].microMIN = 600;
+        servosH[i].microMAX = 2500;
+        servosH[i].angleMIN = 0;
+        servosH[i].angleMAX = 180;
+        servosH[i].movementDelay =  1;
+        servosH[i].movementStep = 1;
+      break;
+
+      case 3:
+        servosH[i].servoPin = SERVO_PIN_3;
+        servosH[i].microMIN = 600;
+        servosH[i].microMAX = 2500;
+        servosH[i].angleMIN = 0;
+        servosH[i].angleMAX = 180;
+        servosH[i].movementDelay =  1;
+        servosH[i].movementStep = 1;
+      break;
+    }
+
+    servosH[i].servo.attach(servosH[i].servoPin);
+    servosH[i].currentAngle = SERVO_START_ANGLE;
+    mapAngleToMicroSeconds(i, SERVO_START_ANGLE);
+  }
+
+  resetServosToInitialPosition();
+}
+
+void resetServosToInitialPosition()
+{
+  for( int i = 0; i < NSERVOS; i++)
+    moveServo(servosH[i].currentAngle, i);
+}
+
+void loop() 
+{  
+  checkButtons();
+
+  currentMillis  = millis();
+  if(currentMillis - previousMillis > 25)
+  {
+    previousMillis = currentMillis;
+    serialRecvCmd(';');
+    serialLogRcvData();
+    
+    if (newData == true)
+    {
+      int newValue    = atoi(strNewValue);
+      int servoNumber = atoi(strServoNum);
+      
+      if( SERVO_MOVEMENT == USE_ABSOLUTE_VALUES )
+        moveServo(newValue, servoNumber);
+      else if( SERVO_MOVEMENT == USE_RELATIVE_VALUES )
+        moveServo(servosH[servoNumber].currentAngle + newValue, servoNumber);
+
+      newData = false;
+    }
+  }
+  
+}
+
+float mapAngleToMicroSeconds(const int angle, const int servoNumber)
+{
+  servosH[servoNumber].microMove = map(angle, 
+                                      servosH[servoNumber].angleMIN, 
+                                      servosH[servoNumber].angleMAX, 
+                                      servosH[servoNumber].microMIN, 
+                                      servosH[servoNumber].microMAX);
+
+  return servosH[servoNumber].microMove;
+}
+
+void microMoveServo( const short servoNumber )
+{
+  servosH[servoNumber].servo.writeMicroseconds(servosH[servoNumber].microMove);
+}
+
+
+void moveServo(const int newAngle, const short servoNumber)
+{
+  char logMsg[128] = {0, };
+
+  if(servosH[servoNumber].currentAngle == newAngle)
+    return;
+
+  sprintf(logMsg, "Moving servo %d from %d to %d", servoNumber, servosH[servoNumber].currentAngle, newAngle);
+  Serial.println(logMsg);
+  if (newAngle >= 0 && newAngle <= 180)
+  {
+    if (newAngle < servosH[servoNumber].currentAngle)
+      for (; servosH[servoNumber].currentAngle > newAngle; servosH[servoNumber].currentAngle -= servosH[servoNumber].movementStep)
+      {
+        servosH[servoNumber].servo.writeMicroseconds(mapAngleToMicroSeconds(servosH[servoNumber].currentAngle, servoNumber));
+        delay(servosH[servoNumber].movementDelay);
+      }
+    else
+      for (; servosH[servoNumber].currentAngle < newAngle; servosH[servoNumber].currentAngle += servosH[servoNumber].movementStep)
+      {
+        servosH[servoNumber].servo.writeMicroseconds(mapAngleToMicroSeconds(servosH[servoNumber].currentAngle, servoNumber));
+        delay(servosH[servoNumber].movementDelay);
+      }
+    servosH[servoNumber].currentAngle = newAngle;
+  }
+  else
+    Serial.println("Invalid angle received");
+
+}
+
+void serialFlush()
+{
+  Serial.println(" >> Flushing serial input <<");
+  while(Serial.available())
+    Serial.read();
+}
+// recv command from serial comm with following format:
+//    VAL-NUMSERVO;
+//    nnn-x;  6 chars
+void serialRecvCmd(char endMarker) 
+{
+  bool   commandValidated = false;
+  bool   angleReceived    = false;
+  static byte ndx = 0;
+  char   rc;
+
+  // if there is a not process data -> flush the incoming serial data
+  if( newData == true )
+    serialFlush();
+
+  while (Serial.available() > 0 && newData == false) 
+  {
+    rc = Serial.read();
+
+    if( commandValidated == false && rc != (char)(USE_RELATIVE_VALUES + '0') )
+    {
+      Serial.print(" > Invalid command received ");
+      Serial.println(rc);
+      serialFlush();
+      break;
+    }
+    commandValidated = true;
+    // skip the command info
+
+    if (rc != endMarker) 
+    {
+      receivedChars[ndx] = rc;
+      ndx++;
+
+      if( angleReceived == false && rc == '#' )
+      {
+        angleReceived = true;
+        strncpy(strNewValue, &receivedChars[1], ndx-1);
+        strNewValue[ndx-1] = 0;
+        memset(receivedChars, 0, sizeof(receivedChars));
+        ndx = 0;
+      }
+      
+      if (ndx >= numChars) 
+        ndx = numChars - 1;
+    }
+    else 
+    {
+      receivedChars[ndx] = '\0'; // terminate the string
+      strcpy(strServoNum, receivedChars);
+      ndx = 0;
+      newData = true;
+    }
+  }
+}
+
+void serialLogRcvData() 
+{
+  if (newData == true) 
+  {
+    Serial.print("This just in ... ");
+    Serial.print(strNewValue);
+    Serial.print(" ==> ");
+    Serial.println(strServoNum);
+    if( atoi(strServoNum) >= NSERVOS)
+    {
+      Serial.print("Invalid servo number received. ");
+      newData = false;
+    }
+  }
+}
+
+void checkButtons()
+{
+  int redButtonState   = digitalRead(BTN_RED);
+  int blackButtonState = digitalRead(BTN_BLACK);
+
+  if( redButtonState == LOW )
+  {
+    Serial.println(" > RED BUTTON PRESSED");
+    //delay(50);
+    moveServo(servosH[0].currentAngle+10, 0);
+  }
+
+  if( blackButtonState == LOW )
+  {
+    Serial.println(" > BLACK BUTTON PRESSED");
+    moveServo(servosH[0].currentAngle-10, 0);
+    //delay(50);
+  }
+
+}
