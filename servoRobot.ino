eecbdc01ecaeb007c27d73285c9b2ccf4f6fb401
smeul <smeualex@gmail.com>
2017-03-20 00:12:38 +0200

    Created servo handler header

--- servoRobot.ino
+++ servoRobot.ino
@@ -1,6 +1,6 @@
 #include <Servo.h>
 #include <Logging.h>
-
+#include "servoHndl.h"
 
 #define BUTTON_1_PIN 10
 #define BUTTON_2_PIN 16
@@ -19,33 +19,6 @@ char strServoNum[2];
 char servoMovement = USE_RELATIVE_VALUES;
 
 boolean newData = false;
-
-#define SERVO_PIN_0 6;
-#define SERVO_PIN_1 7;
-#define SERVO_PIN_2 8;
-#define SERVO_PIN_3 9;
-
-#define SERVO_START_ANGLE 90
-
-#define NSERVOS 4
-struct ServoHndl
-{
-#define MAX_HIST_STEPS 25
-  Servo servo;
-  bool  isMoving;
-  int   servoPin;
-  short currentAngle;
-  
-  float microMove;
-  float microMIN;
-  float microMAX;
-  int   angleMIN;
-  int   angleMAX;
-
-  short movementDelay;
-  short movementStep;
-//  short angleHistory[MAX_HIST_STEPS];
-} servosH[NSERVOS];
 
 unsigned long currentMillis  = millis();
 unsigned long previousMillis = 0;
