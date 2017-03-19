b931c50fcf6a6b395aba121af5c20cca2f813708
smeul <smeualex@gmail.com>
2017-03-20 00:13:51 +0200

    Refactor header

--- servoHndl.h
+++ servoHndl.h
@@ -1,14 +1,15 @@
+/**
+* ARDUINO PINS
+*/
 #define SERVO_PIN_0 6;
 #define SERVO_PIN_1 7;
 #define SERVO_PIN_2 8;
 #define SERVO_PIN_3 9;
 
-#define SERVO_START_ANGLE 90
-
 #define NSERVOS 4
+#define SERVO_START_ANGLE 90
 struct ServoHndl
 {
-#define MAX_HIST_STEPS 25
   Servo servo;
   bool  isMoving;
   int   servoPin;
@@ -22,5 +23,4 @@ struct ServoHndl
 
   short movementDelay;
   short movementStep;
-//  short angleHistory[MAX_HIST_STEPS];
 } servosH[NSERVOS];
