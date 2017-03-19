4b0b5a9b03e5ace18b72f0a6da1f69d44e9af8e4
smeul <smeualex@gmail.com>
2017-03-19 21:43:37 +0200

    Added Arduino replies in logs

--- serialSendFromFile.py
+++ serialSendFromFile.py
@@ -6,7 +6,10 @@ ser = serial.Serial('COM5', 115200)
 def serialSend(stringData):
     print("sending cmd - " + stringData)
     ser.write(bytes(stringData.encode('ascii')))
-    time.sleep(0.05)
+    
+    bytesToRead = ser.inWaiting()
+    dataRead = ser.read(bytesToRead)
+    print (" > " + str(dataRead))
 
 with open('./logs/1_serialCommands.out') as f:
     commands = f.readlines()
